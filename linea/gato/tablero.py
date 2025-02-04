c87e1127366dfe95b4e9fac0b870beb5e0b358dd
Aron-Iniguez <a223216534@unison.mx>
2025-02-04 14:40:28 -0700

    Se agregó funciones en juego

--- linea/gato/tablero.py
+++ linea/gato/tablero.py
@@ -13,16 +13,66 @@ def dibuja_tablero(simbolos:dict):
     {simbolos['7']}  |  {simbolos['8']}  |  {simbolos['9']}
     ''')
 
+def ia(simbolos:dict):
+    ''' Estrategia de la computadora'''
+    ocupado = True
+    while ocupado is True:
+        x = random.choice(list(simbolos.keys()))
+        if simbolos[x] not in ['X','O']:
+            simbolos[x] = 'O'
+            ocupado = False
+
+def usuario(simbolos:dict):
+    '''Estrategía de Usuario'''
+    ocupado = True
+    lista_numeros = [str(i) for i in range (1,10)] #del 1 al 9]
+    while ocupado is True:
+        x = input('Elija un numero del 1 al 9: ')
+        if x in lista_numeros:
+            if simbolos[x] not in ['X','O']:
+                simbolos[x] = 'X'
+                ocupado = False
+            else:
+                print('Esa casilla esta ocupada')
+
+def juego(simbolos:dict):
+    '''juego del gato'''
+    lista_combinaciones = [
+        ['1','2','3'],
+        ['4','5','6'],
+        ['7','8','9'],
+        ['1','4','7'],
+        ['2','5','8'],
+        ['3','6','9'],
+        ['1','5','9'],
+        ['3','5','7']
+    
+        ]
+    en_juego = True
+    while en_juego:
+         usuario(simbolos)
+         dibuja_tablero(simbolos)
+         gana = checa_winner(simbolos, lista_combinaciones)
+         if gana is not None:
+             en_juego = False
+         ia(simbolos)
+         dibuja_tablero(simbolos)
+         gana = checa_winner(simbolos, lista_combinaciones)
+         if gana is not None:
+             en_juego = False
+
+def checa_winner(simbolos:dict, combinaciones:list):
+    '''checa si hay un ganador'''
+    for c in combinaciones:
+        if simbolos[c[0]] == simbolos[c[1]] == simbolos[c[2]]:
+            return simbolos[c[0]]
+    return None    
+
 if __name__ == '__main__':
-    numeros = [str(i) for i in range(1,10)]
-    dsimbolos = {x:x for x in numeros}
-    dibuja_tablero(dsimbolos)
-    x = random.choice(numeros)
-    numeros.remove(x)
-    dsimbolos[x] = 'X'
-    dibuja_tablero(dsimbolos)
-    o = random.choice(numeros)
-    numeros.remove(o)
-    dsimbolos[0] = 'O'
-    dibuja_tablero(dsimbolos)
-    print(numeros)
+        numeros = [str(i) for i in range(1,10)]
+        dsimbolos = {x:x for x in numeros}
+        dibuja_tablero(dsimbolos)
+        ia(dsimbolos)
+        dibuja_tablero(dsimbolos)
+        usuario(dsimbolos)
+        dibuja_tablero(dsimbolos)
