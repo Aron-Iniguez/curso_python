5e2192a0ceded206a416a06baaa3ec1c5b3dac37
Aron-Iniguez <a223216534@unison.mx>
2025-03-05 14:47:02 -0700

    Corrección de errores en app.py de Hello_World

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,27 +1,30 @@
-''' Hola mundo de Flask '''
+''' Hola mundo de Flask'''
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/') #Home page o raíz o índice
 def index():
+    '''Función que se ejecuta cuando se accede a la raíz de la página'''
     return '''<html>
                     <head>
-                        <tittle><body><h1><Hola World>
+                        <title>Hello world</title>
                     </head>
-                        <body><h1>Hola mundo</h1>
-                        <p>Ir a la página de <a href="/about>Acerca de</a></p>
-                        </body>
-                </html>'''
+                    <body><h1>Hola mundo</h1>
+                        <p>Ir a la página de <a href="/about">Acerca de</a></p>
+                    </body>
+            </html>'''
 
-def index():
+@app.route('/about') #Acerca de
+def about():
+    '''Función que se ejecuta cuando se accede a la página de acerca de'''
     return '''<html>
                     <head>
-                        <tittle><body><h1><Hola World>
+                        <title>Acerca de</title>
                     </head>
-                        <body><h1>Hola mundo</h1>
-                        <p>Ir a la página de <a href="/Inicio</a></p>
-                        </body>
-                </html>'''
+                    <body><h1>Acerca de</h1>
+                        <p>Ir a la página de <a href="/">Inicio</a></p>
+                    </body>
+            </html>'''
 
 if __name__ == '__main__':
     app.run(debug=True) #Activar el modo de depuración
