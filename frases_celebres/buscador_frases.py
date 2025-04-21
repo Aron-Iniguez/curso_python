61f8045bee87a28d3138a1acd219ee456c1b6605
Aron-Iniguez <a223216534@unison.mx>
2025-04-21 16:18:02 -0700

    buscador_frases.py ahora resalta la palabra a buscar

--- frases_celebres/buscador_frases.py
+++ frases_celebres/buscador_frases.py
@@ -9,17 +9,23 @@ def lee_archivo_csv(archivo:str)->list:
         return [x for x in csv.DictReader(f)]
 
 def busca_frases_con_palabra(lista:list, palabra:str)->list:
-    ''' Busca frases que contengan una palabra específica '''
-    palabra = palabra.lower()
+    ''' Busca frases que contengan una palabra específica y la resalta en mayúsculas '''
+    palabra_lower = palabra.lower()
     resultado = []
     for fila in lista:
-        if palabra in fila['frase'].lower():
-            resultado.append({'frase': fila['frase'], 'pelicula': fila['pelicula']})
+        frase = fila['frase']
+        palabras = frase.split()
+        frase_resaltada = ' '.join([
+            p.upper() if palabra_lower in p.lower() else p for p in palabras
+        ])
+        if palabra_lower in frase.lower():
+            resultado.append({'frase': frase_resaltada, 'pelicula': fila['pelicula']})
     return resultado
 
 if __name__ == '__main__':
     archivo_csv = 'frases.csv'
     lista_palabras = lee_archivo_csv(archivo_csv)
-    resultado = busca_frases_con_palabra(lista_palabras, 'hace')
+    palabra_buscada = 'hace'
+    resultado = busca_frases_con_palabra(lista_palabras, palabra_buscada)
     for r in resultado:
         print(f"{r['frase']} ({r['pelicula']})")
